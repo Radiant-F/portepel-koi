9c3896441c48ef9e2b9b9c5b0965af65ce084be0
Radiant-F <radiantfadilah0@gmail.com>
2022-05-27 16:00:39 +0700

    add: 4 about

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,7 +1,61 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
 
 import "./About.scss";
 
+const abouts = [
+  {
+    title: "Mobile Development",
+    description: "Scalability as it finest.",
+    imgUrl: images.about01,
+  },
+  {
+    title: "UI/UX",
+    description:
+      "Sending messages from UI to client in a beautiful way and polished manner to achive amazing UX.",
+    imgUrl: images.about02,
+  },
+  {
+    title: "Backend Development",
+    description: "As structured as possible, as cleanly as possible.",
+    imgUrl: images.about03,
+  },
+  {
+    title: "MERN Stack",
+    description: "The stack I mostly use.",
+    imgUrl: images.about04,
+  },
+];
+
 export default function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <h2 className="head-text">
+        Doing <span>a Great Work</span>
+        <br />
+        Is to <span> Love What You Do</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 }
