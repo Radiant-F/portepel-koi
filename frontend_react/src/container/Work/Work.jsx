c023b08444d58f72bc053bb0bf4429dc6851ae18
Radiant-F <radiantfadilah0@gmail.com>
2022-05-28 14:32:22 +0700

    add: works category

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+import { AppWrap } from "../../wrapper/export";
+import { urlFor, client } from "../../client";
 
 import "./Work.scss";
 
 export default function Work() {
-  return <div>Work</div>;
+  const [activeFilter, setActiveFilter] = useState("All");
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
+
+  useEffect(() => {
+    client
+      .fetch('*[_type == "works"]')
+      .then((data) => {
+        setWorks(data);
+        setFilterWork(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleWorkFilter = (item) => {};
+
+  return (
+    <>
+      <h2 className="head-text">
+        My <span>Creative Portfolio</span> Section
+      </h2>
+      <div>
+        {["UI/UX", "Web App", "Mobile App", "React JS", "All"].map(
+          (item, index) => (
+            <div
+              key={index}
+              onClick={() => handleWorkFilter(item)}
+              className={`app__work-filter-item app__flex p-text ${
+                activeFilter === item && "item-active"
+              }`}
+            >
+              {item}
+            </div>
+          )
+        )}
+      </div>
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__work-portfolio"
+      >
+        {filterWork.map((work, index) => (
+          <div className="app__work-item app__flex" key={index}>
+            <div className="app__work-img app__flex">
+              <img src={urlFor(work.imgUrl)} alt={work.name} />
+            </div>
+          </div>
+        ))}
+      </motion.div>
+    </>
+  );
 }
