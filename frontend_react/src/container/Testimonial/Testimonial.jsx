6b4b16b41636daff54c1c1d81b44a97a8342f0c7
Radiant-F <radiantfadilah0@gmail.com>
2022-05-31 13:02:06 +0700

    fetching testimonial

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
+import { AppWrap, MotionWrap } from "../../wrapper/export";
+import { urlFor, client } from "../../client";
 
 import "./Testimonial.scss";
 
-export default function Testimonial() {
-  return <div>Testimonial</div>;
+function Testimonial() {
+  const [brands, setBrands] = useState([]);
+  const [testimonials, setTestimonials] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    client
+      .fetch('*[_type == "testimonials"]')
+      .then((data) => setTestimonials(data))
+      .catch((error) => console.log(error));
+    client
+      .fetch('*[_type == "brands"]')
+      .then((data) => setBrands(data))
+      .catch((error) => console.log(error));
+  }, []);
+  return (
+    <>
+      {testimonials.length && (
+        <>
+          <div className="app__testimonial-item app__flex">
+            <img
+              src={urlFor(testimonials[currentIndex].imgurl)}
+              alt="testimonial"
+            />
+          </div>
+        </>
+      )}
+    </>
+  );
 }
+
+export default AppWrap(
+  MotionWrap(Testimonial, "app__testimonial"),
+  "testimonial",
+  "app__primarybg"
+);
