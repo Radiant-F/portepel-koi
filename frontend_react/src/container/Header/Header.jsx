29c0121439142dbc6a0461986bf1123f2203d361
Radiant-F <radiantfadilah0@gmail.com>
2022-06-06 15:43:28 +0700

    change skill order

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -27,14 +27,14 @@ function Header() {
           <div className="badge-cmp app__flex">
             <span>👋</span>
             <div style={{ marginLeft: 20 }}>
-              <p className="p-text">Hello,I am</p>
+              <p className="p-text">Hello, I am</p>
               <h1 className="head-text">Radiant</h1>
             </div>
           </div>
 
           <div className="tag-cmp app__flex">
             <p className="p-text">Mobile Developer</p>
-            <p className="p-text">Fullstack MERN</p>
+            <p className="p-text">&& MERN Stack</p>
           </div>
         </div>
       </motion.div>
@@ -57,7 +57,7 @@ function Header() {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-        {[images.flutter, images.redux, images.sass].map((circle, index) => (
+        {[images.node, images.react, images.redux].map((circle, index) => (
           <div className="circle-cmp app__flex" key={`circle-${index}`}>
             <img src={circle} alt="circle" />
           </div>
